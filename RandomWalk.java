84b6fcf09349f7491687c66d71a830257875e693
zeffy <zeffy@users.noreply.github.com>
2015-03-18 23:40:33 -0700

    size,size to 0,0 etc

--- RandomWalk.java
+++ RandomWalk.java
@@ -13,11 +13,13 @@ public class RandomWalk {
     private Random rand;
     private ArrayList<Point> walk;
     private Point last;
+    private int size;
 
     public RandomWalk(int size, long seed) {
         rand = new Random(seed);
         walk = new ArrayList<Point>();
-        last = new Point(size - 1, size - 1);
+        last = new Point(0, 0);
+        this.size = size - 1;
         walk.add(last);
     }
 
@@ -27,21 +29,21 @@ public class RandomWalk {
 
     public boolean isDone() {
         Point p = last;
-        return (p.x == 0 && p.y == 0);
+        return (p.x == size && p.y == size);
     }
 
     public void step() {
         if (!this.isDone()) {
             int choice = rand.nextInt(2);
             //System.out.println("Choice = " + choice);
-            if (choice == 0 && last.y > 0) {                      // go up
-                last = new Point(last.x, last.y - 1);
-            } else if (choice == 1 && last.x > 0) {                                // go left
-                last = new Point(last.x - 1, last.y);
-            } else if (choice == 0 && last.y == 0) {
-                last = new Point(last.x - 1, last.y);
-            } else if (choice == 1 && last.x == 0) {
-                last = new Point(last.x, last.y - 1);
+            if (choice == 0 && last.y < size) {                      // go up
+                last = new Point(last.x, last.y + 1);
+            } else if (choice == 1 && last.x < size) {                                // go left
+                last = new Point(last.x + 1, last.y);
+            } else if (choice == 0 && last.y == size) {
+                last = new Point(last.x + 1, last.y);
+            } else if (choice == 1 && last.x == size) {
+                last = new Point(last.x, last.y + 1);
             }
             walk.add(last);
         }
