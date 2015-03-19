d6020a8874d64f819adc7caffcce99257016f130
zeffy <zeffy@users.noreply.github.com>
2015-03-18 23:27:17 -0700

    fix offset bug

--- RandomWalk.java
+++ RandomWalk.java
@@ -6,19 +6,18 @@ import java.awt.Point;
  * Example RandomWalk created in 5 march lecture
  *
  * @author Ian Truslow
- *
  */
 
 public class RandomWalk {
 
-	private Random rand;
-	private ArrayList<Point> walk;
+    private Random rand;
+    private ArrayList<Point> walk;
     private Point last;
 
     public RandomWalk(int size, long seed) {
         rand = new Random(seed);
         walk = new ArrayList<Point>();
-        last = new Point(size, size);
+        last = new Point(size - 1, size - 1);
         walk.add(last);
     }
 
@@ -26,33 +25,34 @@ public class RandomWalk {
         this(size, 0);
     }
 
-	public boolean isDone() {
-		Point p = last;
-		return (p.x == 0 && p.y == 0);
-	}
+    public boolean isDone() {
+        Point p = last;
+        return (p.x == 0 && p.y == 0);
+    }
 
-	public void step() {
-		if (!this.isDone()) {
-			int choice = rand.nextInt(2);
-			//System.out.println("Choice = " + choice);
-			if (choice == 0) {                      // go up
-				last = new Point(last.x, last.y - 1);
-			} else {                                // go left
+    public void step() {
+        if (!this.isDone()) {
+            int choice = rand.nextInt(2);
+            //System.out.println("Choice = " + choice);
+            if (choice == 0) {                      // go up
+                last = new Point(last.x, last.y - 1);
+            } else {                                // go left
                 last = new Point(last.x - 1, last.y);
-			}
+            }
             walk.add(last);
-		}
-	}
+        }
+        System.out.println(this.toString());
+    }
 
-	public ArrayList<Point> getPath() {
-		return walk;
-	}
+    public ArrayList<Point> getPath() {
+        return walk;
+    }
 
-	public String toString() {
-		String rVal = "";
-		for (Point p : walk) {
-			rVal += "[" + p.x + "," + p.y + "] ";
-		}
-		return rVal;
-	}
+    public String toString() {
+        String rVal = "";
+        for (Point p : walk) {
+            rVal += "[" + p.x + "," + p.y + "] ";
+        }
+        return rVal;
+    }
 }
